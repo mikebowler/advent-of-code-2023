cd3f215404e476c945122e62c7491bd796395374
Mike Bowler <mbowler@gargoylesoftware.com>
2023-12-03 15:25:16 -0800

    day 2 part 2

--- lib/day02/day02.ex
+++ lib/day02/day02.ex
@@ -4,37 +4,76 @@ defmodule Day02 do
     {:ok, contents} = File.read("lib/day02/data.txt")
     total = contents
     |> String.split("\n", trim: true) \
-    |> Enum.map(&parse_line/1)
+    |> Enum.map(&get_game_number_for_valid_games/1)
     |> Enum.filter(fn game_number -> game_number != :not_valid end)
     |> Enum.sum
 
-    IO.puts(total)
+    IO.puts("part 1: #{total}")
+    total
   end
 
-  def parse_line(line) do
+  def run_part2 do
+    {:ok, contents} = File.read("lib/day02/data.txt")
+    total = contents
+    |> String.split("\n", trim: true)
+    |> Enum.map(fn line -> cubes_required_for(line) end)
+    |> Enum.map(fn x -> x[:red] * x[:green] * x[:blue] end)
+    |> Enum.sum
+
+    IO.puts("part 2: #{total}")
+    total
+  end
+
+  def get_game_number_for_valid_games(line) do
+    {game_number, handfuls} = parse_game_line(line)
+
+    if Enum.all?(handfuls, fn handful -> is_handful_possible(handful) end) do
+      game_number
+    else
+      :not_valid
+    end
+  end
+
+  def parse_game_line(line) do
     [game_label, games_line] = String.split(line, ": ")
     "Game " <> game_number = game_label
 
-    is_valid = games_line
+    handfuls = games_line
     |> String.split("; ")
-    |> Enum.all?(fn handful -> is_handful_possible(parse_handful_line(handful)) end)
+    |> Enum.map(fn handful -> parse_handful_line(handful) end)
 
-    if is_valid, do: String.to_integer(game_number), else: :not_valid
+    {String.to_integer(game_number), handfuls}
   end
 
   # Converts a line like "8 green, 6 blue, 20 red" to {green: 8, blue: 6, red:20}
   def parse_handful_line(line) do
-    line
+    scores = line
     |> String.split(", ")
     |> Enum.map( fn chunk -> 
       [score, label] = String.split(chunk, " ")
       {String.to_atom(label), String.to_integer(score)}
     end )
     |> Enum.into(%{})
+
+    Map.merge(%{red: 0, green: 0, blue: 0}, scores)
   end
 
   def is_handful_possible(scores) do
-    expanded_scores = Map.merge(%{red: 0, green: 0, blue: 0}, scores)
-    expanded_scores[:red] <= 12 && expanded_scores[:green] <= 13 && expanded_scores[:blue] <= 14
+    scores[:red] <= 12 && scores[:green] <= 13 && scores[:blue] <= 14
+  end
+
+  defp max_value_for_key(handfuls, key) do
+    handfuls
+    |> Enum.map(fn x -> x[key] end)
+    |> Enum.max
+  end
+
+  def cubes_required_for(game_line) do
+    {game_number, handfuls} = parse_game_line(game_line)
+    %{
+      blue: max_value_for_key(handfuls, :blue),
+      red: max_value_for_key(handfuls, :red),
+      green: max_value_for_key(handfuls, :green),
+    }
   end
 end
--- spec/day02_spec.exs
+++ spec/day02_spec.exs
@@ -19,23 +19,46 @@ defmodule Day02Spec do
 
     example "parse valid game" do
       line = "Game 3: 8 green, 6 blue, 2 red; 5 blue, 4 red, 13 green; 5 green, 1 red"
-      Day02.parse_line(line)
+      Day02.get_game_number_for_valid_games(line)
       |> to(eql(3))
     end
 
     example "parse invalid game" do
       line = "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red"
-      Day02.parse_line(line)
+      Day02.get_game_number_for_valid_games(line)
       |> to(eql(:not_valid))
     end
   end  
 
   context "part 2" do
+
+    it "should parse game line" do
+      Day02.parse_game_line("Game 3: 8 green, 6 blue, 2 red; 5 blue, 4 red, 13 green; 5 green, 1 red")
+      |> to(eql({
+        3, 
+        [
+          %{green: 8, red: 2, blue: 6},
+          %{green: 13, red: 4, blue: 5},
+          %{green: 5, red: 1, blue: 0}
+        ]
+      }))
+    end
+
+    example "cubes required" do
+      Day02.cubes_required_for("Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green")
+      |> to(eql(%{green: 2, blue: 6, red: 4}))
+    end
   end
 
-  example "Run against prod data to get final answer" do
-    Day02.run_part1
-  end
+  # context "running against prod data" do
+  #   example "Part 1" do
+  #     Day02.run_part1
+  #     |> to(eql(2683))
+  #   end
 
-
+  #   example "Part 2" do
+  #     Day02.run_part2
+  #     |> to(eql(49710))
+  #   end
+  # end 
 end
